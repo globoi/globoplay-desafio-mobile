c1d93076ad1fac8a08d719b8362ec7191da000fd
fwgusberti <fwgusberti@gmail.com>
2020-03-04 20:38:07 -0300

    First commit

---
+++ themoviedbapp/app/src/main/java/br/com/nerdrapido/themoviedbapp/data/model/DiscoverMovieObject.kt
@@ -0,0 +1,19 @@
+package br.com.nerdrapido.themoviedbapp.data.model
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * Created By FELIPE GUSBERTI @ 03/03/2020
+ *
+ * Ref https://developers.themoviedb.org/3/discover/movie-discover
+ */
+data class DiscoverMovieObject(
+    @SerializedName("page")
+    val page : Int?,
+    @SerializedName("results")
+    val results : List<MovieListResultObject>?,
+    @SerializedName("total_results")
+    val totalResults : Int?,
+    @SerializedName("total_pages")
+    val totalPages : Int?
+)
---
+++ themoviedbapp/app/src/main/java/br/com/nerdrapido/themoviedbapp/data/repository/discover/DiscoverRepository.kt
@@ -0,0 +1,9 @@
+package br.com.nerdrapido.themoviedbapp.data.repository.discover
+
+/**
+ * Created By FELIPE GUSBERTI @ 03/03/2020
+ */
+interface DiscoverRepository {
+
+
+}
