476c5df1c79767fe61524057e28538edbc77e5cb
fwgusberti <fwgusberti@gmail.com>
2020-03-16 01:55:49 -0300

    MOvie list + video visualization

--- themoviedbapp/app/src/main/java/br/com/nerdrapido/themoviedbapp/ui/components/horizontalmovielist/HorizontalMovieAdapter.kt
+++ themoviedbapp/app/src/main/java/br/com/nerdrapido/themoviedbapp/ui/components/horizontalmovielist/HorizontalMovieAdapter.kt
@@ -1,7 +1,10 @@
 package br.com.nerdrapido.themoviedbapp.ui.components.horizontalmovielist
 
 import android.content.Context
+import android.util.DisplayMetrics
 import android.view.View
+import android.view.ViewGroup
+import android.view.Window
 import br.com.nerdrapido.themoviedbapp.R
 import br.com.nerdrapido.themoviedbapp.data.model.common.MovieListResultObject
 import br.com.nerdrapido.themoviedbapp.ui.components.abstracts.MovieListAdapter
@@ -19,4 +22,12 @@ class HorizontalMovieAdapter(
         HorizontalMovieViewHolder(itemView)
 
     override fun getViewHolderLayoutId() = R.layout.view_movie_card_horizontal
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HorizontalMovieViewHolder {
+        val view = super.onCreateViewHolder(parent, viewType)
+        val displayMetrics = context.resources.displayMetrics
+        val height = displayMetrics.heightPixels
+        view.poster.layoutParams.height = height / 4
+        return view
+    }
 }
