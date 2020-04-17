bf947dfa4ed060c203a7b4db858a54591a50746d
Marcos Curvello <mrcurvello@gmail.com>
2020-04-17 17:56:20 -0300

    Feature preliminary view setup

---
+++ globoplay/View/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  globoplay
+//
+//  Created by Marcos Curvello on 17/04/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @EnvironmentObject private var store: Store
+    
+    var body: some View {
+        Group {
+            if !store.loaded {
+                // todo loader
+            } else {
+                TabView {
+                    NavigationView {
+                        HomeView()
+                            .environmentObject(Store())
+                    }.tabItem {
+                        Image(systemName: "house.fill")
+                        Text("Início")
+                    }
+                    NavigationView {
+                        HomeView()
+                            .environmentObject(Store())
+                    }.tabItem {
+                        Image(systemName: "star.fill")
+                        Text("Minhas lista")
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(Store())
+    }
+}
