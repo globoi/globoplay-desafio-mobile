5a58cc6022ebf6028ece30a577aa171b3ed4efe8
Marcos Curvello <mrcurvello@gmail.com>
2020-04-17 16:27:12 -0300

    Fix add optional id property

--- globoplay/Model/Discover.swift
+++ globoplay/Model/Discover.swift
@@ -9,12 +9,14 @@
 import Foundation
 
 struct Discover<A: Codable>: Codable {
+    var id: Int?
     var page: Int
     var totalResults: Int
     var totalPages: Int
     var results: [A]
     
     enum CodingKeys: String, CodingKey {
+        case id
         case page
         case totalResults = "total_results"
         case totalPages = "total_pages"
