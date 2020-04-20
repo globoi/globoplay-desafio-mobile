d55869bec63b3f33ff71bea40e1f951895f02af4
Marcos Curvello <mrcurvello@gmail.com>
2020-04-20 16:57:32 -0300

    Refactor adopt movie detailable property

--- globoplay/ViewHelpers/InfoView.swift
+++ globoplay/ViewHelpers/InfoView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct InfoView: View {
     @State private var visibleTab = false
-    var movie: MovieResult
+    var movie: Movie
     var height: CGFloat = 300
     
     var body: some View {
@@ -45,41 +45,14 @@ struct InfoView: View {
                             .padding(.top, 30)
                             .padding(.bottom, 18)
                         
-                        HStack(alignment: .top, spacing: 3) {
-                            Text("Título Original:")
-                                .fontWeight(.bold)
-                            Text(self.movie.originalTitle)
+                        List(self.movie.information) { info in
+                            HStack(alignment: .top, spacing: 5) {
+                                Text(info.name)
+                                    .fontWeight(.bold)
+                                Text(info.value)
+                            }
                         }
                         
-                        HStack(alignment: .top, spacing: 5) {
-                            Text("Gênero:")
-                                .fontWeight(.bold)
-                            Text("\(self.movie.genreIds[0])")
-                        }
-                        
-                        HStack(alignment: .top, spacing: 5) {
-                            Text("Episódios:")
-                                .fontWeight(.bold)
-                            Text("\(self.movie.releaseDate)")
-                        }
-                        
-                        HStack(alignment: .top, spacing: 5) {
-                            Text("Ano de produção:")
-                                .fontWeight(.bold)
-                            Text("\(self.movie.releaseDate)")
-                        }
-                        
-                        HStack(alignment: .top, spacing: 5) {
-                            Text("País:")
-                                .fontWeight(.bold)
-                            Text("\(self.movie.releaseDate)")
-                        }
-                        
-                        HStack(alignment: .top, spacing: 5) {
-                            Text("Direção:")
-                                .fontWeight(.bold)
-                            Text("\(self.movie.releaseDate)")
-                        }
                         Spacer()
                     }
                     .font(Font.system(size: 15))
@@ -89,7 +62,7 @@ struct InfoView: View {
                     .zIndex(self.visibleTab ? 0 : 1)
                     
                     VStack {
-                        MovieRow(title: "", movies: sampleMovies)
+                        ResultRowView(title: "", movies: sampleMovies)
                             .frame(width: geo.size.width, height: self.height)
                     }
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
@@ -107,6 +80,7 @@ struct InfoView: View {
 
 struct DetailTab_Previews: PreviewProvider {
     static var previews: some View {
-        InfoView(movie: sampleMovies[7])
+        InfoView(movie: sampleMovie)
+            .environmentObject(Store())
     }
 }
