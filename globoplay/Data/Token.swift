805108fbe28e9138b3292dc41ce2a5839c9dc37f
Marcos Curvello <mrcurvello@gmail.com>
2020-04-21 00:12:23 -0300

    Feature token structure

---
+++ globoplay/Data/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  globoplay
+//
+//  Created by Marcos Curvello on 21/04/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import Foundation
+
+struct Token: Codable {
+    var value: String
+}
