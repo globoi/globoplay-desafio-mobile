4675418d902dc35e57d66588731b43ab52bed85e
Marcos Curvello <mrcurvello@gmail.com>
2020-04-27 20:39:16 -0300

    Refactor remove extraneous init

--- globoplay/View/DetailView.swift
+++ globoplay/View/DetailView.swift
@@ -12,45 +12,42 @@ import TinyNetworking
 struct DetailView: View {
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     @Environment(\.imageCache) var cache: ImageCache
-    
     @EnvironmentObject var store: Store
     @ObservedObject var resource: Resource<Movie>
-    
     var movie: Movie? { resource.value }
 //    var movie: Movie? { sampleMovie }
-    
     var isFavorite = false
-    
-    init(movieId: Int) {
-        let request: Request = .movie(detail: String(movieId))
-        self.resource = Resource<Movie>(endpoint: Endpoint(json: .get, url: request.url!, headers: request.auth))
+
+    init(movieId id: String) {
+        let details: Request = .movie(detail: id)
+        self.resource = Resource<Movie>(endpoint: Endpoint(json: .get, url: details.url!, headers: details.auth))
     }
     
     var body: some View {
         Group {
             if movie == nil {
-                LoaderView()
+                Loader()
             } else {
                 List {
                     VStack {
                         Spacer()
-                        
                         AsyncImage(
                             url: .image(path: movie?.posterPath ?? ""),
                             cache: cache,
                             placeholder: PlaceholderImage(),
                             configuration: { $0.resizable() }
                         )
-                            .frame(width: 120, height: 180)
-                            .padding(.top, 30)
+                            .frame(width: 108, height: 160)
+                            .padding(.top, 40)
                         
                         VStack {
-                            Text(movie?.title ?? "Fight Club")
+                            Text(movie!.title)
                                 .font(.system(size: 20))
                                 .fontWeight(.bold)
+                                .lineLimit(5)
                                 .padding([.top, .bottom], 5)
                             
-                            Text(movie?.overview ?? "")
+                            Text(movie!.overview ?? "")
                                 .font(.system(size: 14))
                                 .font(.subheadline)
                                 .lineLimit(5)
@@ -65,7 +62,6 @@ struct DetailView: View {
                                     }
                                     .frame(maxWidth: .infinity)
                                     .padding()
-                                        
                                     .foregroundColor(.black)
                                     .background(Color.white)
                                     .cornerRadius(5)
@@ -83,7 +79,6 @@ struct DetailView: View {
                                     }
                                     .frame(maxWidth: .infinity)
                                     .padding()
-                                        
                                     .foregroundColor(.white)
                                     .overlay(
                                         RoundedRectangle(cornerRadius: 5).stroke(Color.white, lineWidth: 1)
@@ -92,20 +87,26 @@ struct DetailView: View {
                             }
                         }
                     }
-                    .foregroundColor(Color("systemGray5"))
+                    .foregroundColor(Color.white)
                     .frame(maxHeight: 410)
                     .background(
                         ImageBackground(path: movie?.posterPath ?? "")
                             .frame(height: 410)
                     )
-                    
-                    InfoStackView(movie: movie, list: sampleMovies)
+
+                    InfoStackView(movie: movie!)
+                        .frame(minHeight: 350)
                         .listRowInsets(EdgeInsets(top: 30, leading: 0, bottom: 0, trailing: 0))
+                    
+                    Spacer()
+
                 }
                 .edgesIgnoringSafeArea(.all)
                 .onAppear {
+                    UITableView.appearance().tableFooterView = UIView()
                     UITableView.appearance().backgroundColor = .black
                     UITableViewCell.appearance().backgroundColor = .clear
+                    UITableView.appearance().separatorStyle = .none
                 }
             }
         }
@@ -114,7 +115,7 @@ struct DetailView: View {
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView(movieId: 76341)
+        DetailView(movieId: sampleMovies.first!.id.stringValue)
             .environmentObject(Store())
     }
 }
