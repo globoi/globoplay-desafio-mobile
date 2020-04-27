868b6a44879384332be95faf85e332ab079ebd7f
Marcos Curvello <mrcurvello@gmail.com>
2020-04-27 20:40:01 -0300

    Feature loader view

---
+++ globoplay/Data/Loader.swift
@@ -0,0 +1,42 @@
+//
+//  LoaderView.swift
+//  globoplay
+//
+//  Created by Marcos Curvello on 24/04/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import SwiftUI
+
+struct ActivityIndicator: UIViewRepresentable {
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        let indicator = UIActivityIndicatorView(style: style)
+        indicator.color = .white
+        return indicator
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
+
+struct Loader: View {
+    var title: String? = nil
+    var body: some View {
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            ActivityIndicator(isAnimating: .constant(true), style: .medium)
+            Text(title == nil ? "" : title!)
+                .foregroundColor(.white)
+                .offset(y: 30)
+        }
+    }}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        Loader()
+    }
+}
