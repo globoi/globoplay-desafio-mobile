d6a13f2f9c52a13f561af72c87f9b8e2090f311b
Marcos Curvello <mrcurvello@gmail.com>
2020-04-27 18:02:06 -0300

    Refactor polish layout

--- globoplay/View/DetailView.swift
+++ globoplay/View/DetailView.swift
@@ -10,81 +10,109 @@ import SwiftUI
 import TinyNetworking
 
 struct DetailView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
+    @Environment(\.imageCache) var cache: ImageCache
+    
     @EnvironmentObject var store: Store
-    @ObservedObject var movie: Resource<Movie>
+    @ObservedObject var resource: Resource<Movie>
+    
+    var movie: Movie? { resource.value }
+//    var movie: Movie? { sampleMovie }
+    
+    var isFavorite = false
     
     init(movieId: Int) {
-        let endpoint: EndpointUrl = .movie(detail: String(movieId))
-        self.movie = Resource<Movie>(endpoint: Endpoint(json: .get, url: endpoint.url!, headers: endpoint.auth))
+        let request: Request = .movie(detail: String(movieId))
+        self.resource = Resource<Movie>(endpoint: Endpoint(json: .get, url: request.url!, headers: request.auth))
     }
     
     var body: some View {
         Group {
-            if movie.value != nil {
+            if movie == nil {
+                LoaderView()
+            } else {
                 List {
-                    VStack(spacing: 5) {
-                        ImageHeader(path: movie.value!.posterPath ?? "")
-                            .frame(height: 242)
-                        
+                    VStack {
                         Spacer()
-                            .frame(height: 20)
                         
-                        Text(movie.value!.title)
-                            .font(Font.system(size: 20))
-                            .fontWeight(.bold)
+                        AsyncImage(
+                            url: .image(path: movie?.posterPath ?? ""),
+                            cache: cache,
+                            placeholder: PlaceholderImage(),
+                            configuration: { $0.resizable() }
+                        )
+                            .frame(width: 120, height: 180)
+                            .padding(.top, 30)
                         
-                        Text(movie.value!.overview ?? "")
-                            .font(Font.system(size: 14))
-                            .font(.subheadline)
-                            .multilineTextAlignment(.leading)
-                            .lineLimit(4)
-                        
-                        Spacer()
-                            .frame(height: 5)
-                        
-                        HStack {
-                            Button(action: {}) {
-                                HStack {
-                                    Image(systemName: "play.fill")
-                                    Text("Assista")
-                                }
-                                .frame(minWidth: 0, maxWidth: .infinity)
-                                .padding()
-                                    
-                                .background(Color.black)
-                                .foregroundColor(.white)
-                                .cornerRadius(5)
-                            }
+                        VStack {
+                            Text(movie?.title ?? "Fight Club")
+                                .font(.system(size: 20))
+                                .fontWeight(.bold)
+                                .padding([.top, .bottom], 5)
                             
-                            Button(action: {
-                                self.store.favorite(self.movie.value!)
-                            }) {
-                                HStack {
-                                    Image(systemName: "star.fill")
-                                    Text("Minha Lista")
+                            Text(movie?.overview ?? "")
+                                .font(.system(size: 14))
+                                .font(.subheadline)
+                                .lineLimit(5)
+                                .multilineTextAlignment(.leading)
+                                .padding(.bottom, 5)
+                            
+                            HStack {
+                                Button(action: {}) {
+                                    HStack {
+                                        Image(systemName: "play.fill")
+                                        Text("Assista")
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                        
+                                    .foregroundColor(.black)
+                                    .background(Color.white)
+                                    .cornerRadius(5)
                                 }
-                                .frame(minWidth: 0, maxWidth: .infinity)
-                                .padding()
-                                .foregroundColor(.black)
-                                .overlay(
-                                    RoundedRectangle(cornerRadius: 5)
-                                        .stroke(Color.black, lineWidth: 1)
-                                )
+                                
+                                Button(action: {}) {
+                                    HStack {
+                                        if self.isFavorite {
+                                            Image(systemName: "checkmark")
+                                            Text("Adicionado")
+                                        } else {
+                                            Image(systemName: "star.fill")
+                                            Text("Minha Lista")
+                                        }
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .padding()
+                                        
+                                    .foregroundColor(.white)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 5).stroke(Color.white, lineWidth: 1)
+                                    )
+                                }
                             }
                         }
                     }
+                    .foregroundColor(Color("systemGray5"))
+                    .frame(maxHeight: 410)
+                    .background(
+                        ImageBackground(path: movie?.posterPath ?? "")
+                            .frame(height: 410)
+                    )
+                    
+                    InfoStackView(movie: movie, list: sampleMovies)
+                        .listRowInsets(EdgeInsets(top: 30, leading: 0, bottom: 0, trailing: 0))
                 }
-                .foregroundColor(.black)
-                .navigationBarHidden(true)
-                .edgesIgnoringSafeArea([.top, .bottom])
-            } else {
-                
+                .edgesIgnoringSafeArea(.all)
+                .onAppear {
+                    UITableView.appearance().backgroundColor = .black
+                    UITableViewCell.appearance().backgroundColor = .clear
+                }
             }
         }
     }
 }
 
-struct MovieDetail_Previews: PreviewProvider {
+struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         DetailView(movieId: 76341)
             .environmentObject(Store())
