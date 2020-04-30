4041ca6a4c2fe86df06549a36a13567dd441234f
Marcos Curvello <mrcurvello@gmail.com>
2020-04-30 05:50:53 -0300

    Feature user favorite movies view

---
+++ globoplay/View/FavoriteMoviesView.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesView.swift
+//  globoplay
+//
+//  Created by Marcos Curvello on 20/04/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import SwiftUI
+
+struct FavoriteMoviesView: View {
+    @EnvironmentObject var store: Store
+    var favorites: [MovieList] { store.favorites }
+    //    var favorites: [MovieList] { sampleMovies }
+    
+    var body: some View {
+        ScrollView(.vertical) {
+            GridStackView(movies: favorites, columns: 3)
+        }
+        .background(Color.backgroundGray)
+    }
+}
+
+struct FavoritesView_Previews: PreviewProvider {
+    static var previews: some View {
+        FavoriteMoviesView()
+            .environmentObject(Store())
+    }
+}
