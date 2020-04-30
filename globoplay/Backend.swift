3ee6e698abf81119865b38b3f952809a530f14a3
Marcos Curvello <mrcurvello@gmail.com>
2020-04-30 05:53:28 -0300

    Refactor map movie genres

--- globoplay/Backend.swift
+++ globoplay/Backend.swift
@@ -1,5 +1,5 @@
 //
-//  api.swift
+//  Backend.swift
 //  globoplay
 //
 //  Created by Marcos Curvello on 20/04/20.
@@ -7,6 +7,54 @@
 //
 
 import Foundation
+
+enum GenreType: Int, CaseIterable {
+    case action = 28
+    case adventure = 12
+    case animation = 16
+    case comedy = 35
+    case crime = 80
+    case documentary = 99
+    case drama = 18
+    case family = 10751
+    case fantasy = 14
+    case history = 36
+    case horror = 27
+    case music = 10402
+    case mystery = 9648
+    case romance = 10749
+    case scifi = 878
+    case tvmovie = 10770
+    case thriller = 53
+    case war = 10752
+    case western = 37
+    
+    var title: String {
+        switch self {
+        case .action: return "Action"
+        case .adventure: return "Adventure"
+        case .animation: return "Animation"
+        case .comedy: return "Comedy"
+        case .crime: return "Crime"
+        case .documentary: return "Documentary"
+        case .drama: return "Drama"
+        case .family: return "Family"
+        case .fantasy: return "Fantasy"
+        case .history: return "History"
+        case .horror: return "Horror"
+        case .music: return "Music"
+        case .mystery: return "Mystery"
+        case .romance: return "Romance"
+        case .scifi: return "Science Fiction"
+        case .tvmovie: return "TV Movie"
+        case .thriller: return "Thriller"
+        case .war: return "War"
+        case .western: return "Western"
+        }
+    }
+    
+    var genre: Genre { Genre(id: self.rawValue, name: title) }
+}
 
 protocol Order {
     var asc: String { get }
