bf55436e47b1e85ee5847af52a771680a7ab2f4a
Marcos Curvello <mrcurvello@gmail.com>
2020-04-30 05:46:05 -0300

    Feature color theme

---
+++ globoplay/ViewHelpers/Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Theme.swift
+//  globoplay
+//
+//  Created by Marcos Curvello on 30/04/20.
+//  Copyright © 2020 Marcos Curvello. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let gray3 = Color("systemGray3")
+    static let gray5 = Color("systemGray5")
+    static let alphaBlack = Color("alphaBlack")
+    static let backgroundGray = Color("backgroundGray")
+    
+    static let primaryColor = Color("")
+    static let secondaryColor = Color("")
+}
