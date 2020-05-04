f61d254eb7b9c865aa9001cea5d3de363e048010
Marcos Curvello <mrcurvello@gmail.com>
2020-05-04 04:02:20 -0300

    Test add complete movie movie

--- globoplayTests/MovieTests.swift
+++ globoplayTests/MovieTests.swift
@@ -11,42 +11,60 @@ import XCTest
 
 class MovieTests: XCTestCase {
     
-    var movie: Movie?
+    var sut: Movie?
     
-    override func setUpWithError() throws {
+    override func setUp() {
         
-        movie = Movie(
+        sut = Movie(
             adult: false,
-            backdropPath: "/8uO0gUM8aNqYLs1OsTBQiXu0fEv.jpg",
-            collection: nil,
-            budget: 3231,
-            genres: [],
-            homepage: nil,
-            id: 1,
-            imdbId: "imdb.com/heyf",
-            originalLanguage: "English",
-            originalTitle: "Forest Gump",
-            overview: "Hello",
-            popularity: 22.1,
-            posterPath: "",
-            productionCompanies: [],
-            productionCountries: [],
-            releaseDate: "",
-            revenue: 12,
-            runtime: 313,
-            spokenLanguages: [],
-            status: "",
-            tagline: "",
-            title: "Forest Gump",
-            video: true,
-            voteAverage: 10.0,
-            voteCount: 50
+            backdropPath: "/jOzrELAzFxtMx2I4uDGHOotdfsS.jpg",
+            collection:
+            MovieCollection(
+                id: 10,
+                name: "Star Wars Collection",
+                posterPath: "/r8Ph5MYXL04Qzu4QBbq2KjqwtkQ.jpg",
+                backdropPath: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg"
+            ),
+            budget: 250000000,
+            genres: [
+                Genre(id: 28, name: "Action"),
+                Genre(id: 12, name: "Adventure"),
+                Genre(id: 878, name: "Science Fiction")
+            ],
+            homepage: "https://www.starwars.com/films/star-wars-episode-ix-the-rise-of-skywalker",
+            id: 181812,
+            imdbId: "tt2527338",
+            originalLanguage: "en",
+            originalTitle: "Star Wars: The Rise of Skywalker",
+            overview: "The surviving Resistance faces the First Order once again as the journey of Rey, Finn and Poe Dameron continues. With the power and knowledge of generations behind them, the final battle begins.",
+            popularity: 151.648,
+            posterPath: "/db32LaOibwEliAmSL2jjDF6oDdj.jpg",
+            productionCompanies: [
+                Company(id: 1, logoPath: "/o86DbpburjxrqAzEDhXZcyE8pDb.png", name: "Lucasfilm", originCountry: "US"),
+                Company(id: 11461, logoPath: "/p9FoEt5shEKRWRKVIlvFaEmRnun.png", name: "Bad Robot", originCountry: "US"),
+                Company(id: 2, logoPath: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png", name: "Walt Disney Picturs", originCountry: "US"),
+                Company(id: 120404, logoPath: nil, name: "British Film Comission", originCountry: "")
+            ],
+            productionCountries: [
+                Country(iso: "US", name: "United States of America")
+            ],
+            releaseDate: "2019-12-18",
+            revenue: 1073604458,
+            runtime: 142,
+            spokenLanguages: [
+                Language(iso: "en", name: "English")
+            ],
+            status: "Released",
+            tagline: "Every generation has a legend",
+            title: "Star Wars: The Rise of Skywalker",
+            video: false,
+            voteAverage: 6.5,
+            voteCount: 4328
         )
-        
     }
     
     func testMovieToMovieObjectConversion_ShouldReturnTrue() {
-        guard let movie = movie else {
+        guard let movie = sut else {
             fatalError()
         }
         
@@ -55,7 +73,7 @@ class MovieTests: XCTestCase {
     }
     
     func testMovieObjectToMovie_ShouldReturnTrue() {
-        guard let movie = movie else {
+        guard let movie = sut else {
             fatalError()
         }
         
@@ -65,12 +83,13 @@ class MovieTests: XCTestCase {
     }
     
     func testMovieDetailInformation_ShouldReturnTrue() {
-        guard let movie = movie else {
+        guard let movie = sut else {
             fatalError()
         }
         
-        let title = movie.title
+        let title = "Star Wars: The Rise of Skywalker"
         let titleInfo = movie.information.first
+        
         XCTAssertTrue(titleInfo?.value == title)
     }
     
