356a895dc3925582e0c279dbfe0878e679452230
Marcos Curvello <mrcurvello@gmail.com>
2020-05-06 02:12:36 -0300

    Task delete extraneous class

--- globoplay/Extensions/URLSession+Endpoint.swift
+++
@@ -1,9 +0,0 @@
-//
-//  URLSession+Endpoint.swift
-//  globoplay
-//
-//  Created by Marcos Curvello on 06/05/20.
-//  Copyright © 2020 Marcos Curvello. All rights reserved.
-//
-
-import Foundation
