dd6b9e60bce4f1ece315d9c4aebd426cab054167
Marcos Curvello <mrcurvello@gmail.com>
2020-05-06 04:25:30 -0300

    Feature network error handling

--- globoplay/Model/Resource.swift
+++ globoplay/Model/Resource.swift
@@ -10,27 +10,54 @@ import Foundation
 import Combine
 import TinyNetworking
 
+enum ResourceError: Error {
+    case description(code: Int, info: ErrorInfo?)
+    case unknown(error: Error)
+}
+
+extension ResourceError {
+    var message: String {
+        switch self {
+        case let .description(_,info): return info?.status_message ?? ""
+        default: return "Uncataloged Error"
+        }
+    }
+}
+
+struct ErrorInfo: Codable {
+    var status_code: Int
+    var status_message: String
+}
+
 final class Resource<A>: ObservableObject {
     @Published var value: A? = nil
-    var objectWillChange: AnyPublisher<A?, Never> = Publishers.Sequence<[A?], Never>(sequence: []).eraseToAnyPublisher()
+    @Published var error: ResourceError? = nil
+    
     let endpoint: Endpoint<A>
-    private var firstLoad = true
+    var cancellable: AnyCancellable?
     
     init(endpoint: Endpoint<A>) {
         self.endpoint = endpoint
-        self.objectWillChange = $value.handleEvents(receiveSubscription: { [weak self] sub in
-            guard let s = self, s.firstLoad else { return }
-            s.firstLoad = false
-            s.reload()
-
-        }).eraseToAnyPublisher()
-    }
-    
-    func reload() {
-        URLSession.shared.load(endpoint) { result in
-            DispatchQueue.main.async {
-                self.value = try? result.get()
-            }
+        self.cancellable = URLSession.shared.load(endpoint)
+            .receive(on: DispatchQueue.main)
+            .mapError { error -> ResourceError in
+                switch (error) {
+                case let error as WrongStatusCodeError:
+                    guard let body = error.responseBody else { return .unknown(error: error) }
+                    let info = try? JSONDecoder().decode(ErrorInfo.self, from: body)
+                    return .description(code: error.statusCode, info: info)
+                default: return .unknown(error: error)
+                }
         }
+        .sink(receiveCompletion: { completion in
+            switch completion {
+            case .finished:
+                break
+            case let .failure(error):
+                self.error = error
+            }
+        }, receiveValue: { value in
+            self.value = value
+        })
     }
 }
