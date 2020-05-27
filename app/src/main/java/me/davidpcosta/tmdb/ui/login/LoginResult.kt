ee1f1aaf49f66e9f86695d719874587413a873f2
David Costa <davidpcosta@gmail.com>
2020-05-27 09:39:42 -0300

    Commit inicial

---
+++ app/src/main/java/me/davidpcosta/tmdb/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package me.davidpcosta.tmdb.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
