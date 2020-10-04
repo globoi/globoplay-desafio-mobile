c0cb3b25d50be00558d033ca8e8153555c8d37f7
themaripool <marielama@outlook.com.br>
2020-10-04 09:30:14 -0300

    [ADD] Base files

---
+++ globoplay-desafio-mobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  globoplay-desafio-mobile
+//
+//  Created by Mariela on 04/10/20.
+//  Copyright © 2020 Mariela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
