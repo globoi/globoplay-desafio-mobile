b2c6bd19eb65708eaabb725ef69ed68d1898b937
Wagner Gil Junior <wagner.junior@dextra-sw.com>
2021-05-01 22:03:31 -0300

    Inital commit: added home and favorites vc and assets

---
+++ globoplay-desafio/globoplay-desafio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  globoplay-desafio
+//
+//  Created by Wagner Junior  on 01/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ globoplay-desafio/globoplay-desafioTests/globoplay_desafioTests.swift
@@ -0,0 +1,33 @@
+//
+//  globoplay_desafioTests.swift
+//  globoplay-desafioTests
+//
+//  Created by Wagner Junior  on 01/05/21.
+//
+
+import XCTest
+@testable import globoplay_desafio
+
+class globoplay_desafioTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
