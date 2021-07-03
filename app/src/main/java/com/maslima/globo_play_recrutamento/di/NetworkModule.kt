3cf120c6d702816bf44abe98203873745efd940b
Matheus Lima <matheusinclima@gmail.com>
2021-07-03 15:46:06 -0300

    fixing the name of package, include network module and repository module

---
+++ app/src/main/java/com/maslima/globo_play_recrutamento/di/NetworkModule.kt
@@ -0,0 +1,31 @@
+package com.maslima.globo_play_recrutamento.di
+
+import com.google.gson.GsonBuilder
+import com.maslima.globo_play_recrutamento.network.MovieService
+import com.maslima.globo_play_recrutamento.network.model.MovieDtoMapper
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object NetworkModule {
+    @Singleton
+    @Provides
+    fun provideMovieMapper(): MovieDtoMapper {
+        return MovieDtoMapper()
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieService(): MovieService {
+        return Retrofit.Builder().baseUrl("https://api.themoviedb.org/3/")
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+            .create(MovieService::class.java)
+    }
+}
