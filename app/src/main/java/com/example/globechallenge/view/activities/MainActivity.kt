9cdd03b6bc6ce676bd7bccdd0cf5aebf71fd5cdb
Renata <renatacristovams@gmail.com>
2021-07-17 01:25:10 -0300

    Placing the viewBinding and creating the splash screen

--- app/src/main/java/com/example/globechallenge/view/activities/MainActivity.kt
+++ app/src/main/java/com/example/globechallenge/view/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.globechallenge
+package com.example.globechallenge.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.globechallenge.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/example/globechallenge/view/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.example.globechallenge.view.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.globechallenge.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Handler(Looper.getMainLooper()).postDelayed({
+            showLoginScreen()}, 3000)
+    }
+
+    private fun showLoginScreen() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+}
