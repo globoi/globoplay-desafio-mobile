e3e87ff52339810f804f2c6dc3d3716b288e8ee2
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-02-22 19:30:38 -0300

    Criar projeto no Xcode

---
+++ Desafio-Globoplay-iOS/Desafio-Globoplay-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Desafio-Globoplay-iOS
+//
+//  Created by Gáudio Ney on 22/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
