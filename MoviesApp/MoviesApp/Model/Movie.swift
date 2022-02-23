34e39f4be48c555f773d5e56ff92ffec411691a1
Gustavo Tiecker <gustavotiecker@gmail.com>
2022-02-22 22:58:55 -0300

    create network

---
+++ MoviesApp/MoviesApp/Model/Movie.swift
@@ -0,0 +1,43 @@
+//
+//  Movie.swift
+//  MoviesApp
+//
+//  Created by Gustavo Tiecker on 22/02/22.
+//
+
+import Foundation
+
+struct Movie: Codable {
+    
+    let adult: Bool
+    let backdropPath: String
+    let genreIDS: [Int]
+    let id: Int
+    let mediaType: String
+    let originalLanguage: String
+    let originalTitle: String
+    let overview: String
+    let popularity: Double
+    let posterPath: String
+    let releaseDate: String
+    let title: String
+    let video: Bool
+    let voteAverage: Double
+    let voteCount: Int
+
+    enum CodingKeys: String, CodingKey {
+        case adult
+        case backdropPath = "backdrop_path"
+        case genreIDS = "genre_ids"
+        case id
+        case mediaType = "media_type"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview, popularity
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case title, video
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
---
+++ MoviesApp/MoviesApp/Network/NetworkManager.swift
@@ -0,0 +1,79 @@
+//
+//  NetworkManager.swift
+//  MoviesApp
+//
+//  Created by Gustavo Tiecker on 22/02/22.
+//
+
+import Foundation
+
+enum HttpMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+enum EndPoint {
+    case listMovies(Int)
+    
+    func getPath() -> String {
+        switch self {
+        case .listMovies(let id):
+            return "/list/\(id)"
+        }
+    }
+    
+    func getHttpMethod() -> HttpMethod {
+        switch self {
+        case .listMovies(let id):
+            return .get
+        }
+    }
+}
+
+enum APIError: String, Error {
+    case invalidRequest = "Invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data receveid from the server was invalid. Please try again."
+}
+
+class NetworkManager {
+    
+    static let shared = NetworkManager()
+    
+    private let apiKey = "030b11596288485007a672e552a72ecc"
+    private let baseURL = "https://api.themoviedb.org/3"
+    
+    private init() {}
+    
+    func request(body: String = "", endPoint: EndPoint, completion: @escaping (Result<Data, APIError>) -> Void) {
+        
+        guard let url = URL(string: baseURL + endPoint.getPath() + "?api_key=" + apiKey) else {
+            completion(.failure(.invalidRequest))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            
+            if let _ = error {
+                completion(.failure(.unableToComplete))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            completion(.success(data))
+        }
+        task.resume()
+    }
+}
