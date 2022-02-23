dea6c1581976eb5f77f9992904006720efb6ee1d
Gáudio Uchôa Ney <gaudio.ney@gmail.com>
2022-02-23 09:58:00 -0300

    Add constraints and color extensions

---
+++ Desafio-Globoplay-iOS/Desafio-Globoplay-iOS/Utils/Extensions.swift
@@ -0,0 +1,105 @@
+//
+//  Extensions.swift
+//  Desafio-Globoplay-iOS
+//
+//  Created by Gáudio Ney on 23/02/22.
+//
+
+import UIKit
+
+extension UIView {
+    func anchor(top: NSLayoutYAxisAnchor? = nil,
+                leading: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                trailling: NSLayoutXAxisAnchor? = nil,
+                paddingTop: CGFloat = 0,
+                paddingLeading: CGFloat = 0,
+                paddingBottom: CGFloat = 0,
+                paddingTrailling: CGFloat = 0,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        
+        if let leading = leading {
+            leadingAnchor.constraint(equalTo: leading, constant: paddingLeading).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let trailling = trailling {
+            trailingAnchor.constraint(equalTo: trailling, constant: -paddingTrailling).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width).isActive = true
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height).isActive = true
+        }
+    }
+    
+    func center(inView view: UIView, yConstant: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: yConstant!).isActive = true
+    }
+    
+    func centerX(inView view: UIView, topAnchor: NSLayoutYAxisAnchor? = nil, bottomAnchor: NSLayoutYAxisAnchor? = nil, paddingTop: CGFloat? = 0, paddingBottom: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        if let topAnchor = topAnchor {
+            self.topAnchor.constraint(equalTo: topAnchor, constant: paddingTop!).isActive = true
+        }
+        if let bottomAnchor = bottomAnchor {
+            self.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -paddingBottom!).isActive = true
+        }
+    }
+    
+    func centerY(inView view: UIView, leadingAnchor: NSLayoutXAxisAnchor? = nil, paddingLeading: CGFloat? = nil, constant: CGFloat? = 0) {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: constant!).isActive = true
+        
+        if let leadingtAnchor = leadingAnchor, let padding = paddingLeading {
+            self.leadingAnchor.constraint(equalTo: leadingtAnchor, constant: padding).isActive = true
+        }
+    }
+    
+    func setDimensions(width: CGFloat, height: CGFloat) {
+        translatesAutoresizingMaskIntoConstraints = false
+        widthAnchor.constraint(equalToConstant: width).isActive = true
+        heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    func addConstraintsToFillView(_ view: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        anchor(top: view.topAnchor, leading: view.leadingAnchor,
+               bottom: view.bottomAnchor, trailling: view.trailingAnchor)
+    }
+}
+
+// MARK: - UIColor
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+        /// Globoplay red:
+        static let customRed = UIColor.rgb(red: 230, green: 50, blue: 62)
+        /// Buttons end sections white color:
+        static let customWhite = UIColor.rgb(red: 255, green: 255, blue: 255)
+        /// Globoplay yellow:
+        static let customYellow = UIColor.rgb(red: 240, green: 149, blue: 55)
+        /// Main home Backgroud color:
+        static let homeBlack = UIColor.rgb(red: 31, green: 31, blue: 31)
+    
+}
