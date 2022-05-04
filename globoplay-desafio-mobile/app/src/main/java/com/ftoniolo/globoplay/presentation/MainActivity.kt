57d0de321de2b73e78a6a200cd88a4947577e0a3
Felipe Toniolo <ftoniolo.aat@gmail.com>
2022-05-03 23:18:33 -0300

    configure navigation container in project

--- globoplay-desafio-mobile/app/src/main/java/com/ftoniolo/globoplay/presentation/MainActivity.kt
+++ globoplay-desafio-mobile/app/src/main/java/com/ftoniolo/globoplay/presentation/MainActivity.kt
@@ -2,11 +2,26 @@ package com.ftoniolo.globoplay.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.ftoniolo.globoplay.R
+import com.ftoniolo.globoplay.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
