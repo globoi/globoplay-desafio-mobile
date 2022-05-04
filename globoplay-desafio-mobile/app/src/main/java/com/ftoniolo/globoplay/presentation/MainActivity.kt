019b741b50bd93364546e5a30bbcb02fdb940adf
Felipe Toniolo <ftoniolo.aat@gmail.com>
2022-05-04 03:53:48 -0300

    configured navigation base

--- globoplay-desafio-mobile/app/src/main/java/com/ftoniolo/globoplay/presentation/MainActivity.kt
+++ globoplay-desafio-mobile/app/src/main/java/com/ftoniolo/globoplay/presentation/MainActivity.kt
@@ -2,11 +2,16 @@ package com.ftoniolo.globoplay.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.ftoniolo.globoplay.R
 import com.ftoniolo.globoplay.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
@@ -19,9 +24,33 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.nav_host_container) as NavHostFragment
 
+        val navView: BottomNavigationView = binding.bottomNavMain
+
+        navView.menu.findItem(R.id.menu_disable1)
+            .isEnabled = false
+        navView.menu.findItem(R.id.menu_disable2)
+            .isEnabled = false
+        navView.menu.findItem(R.id.home)
+            .isChecked = true
+
         navController = navHostFragment.navController
+        navView.setupWithNavController(navController)
         appBarConfiguration = AppBarConfiguration(
-            setOf()
+            setOf(R.id.homeFragment, R.id.favoritesFragment)
         )
+
+        binding.toolbarApp.setupWithNavController(navController, appBarConfiguration)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val isTopLevelDestination =
+                appBarConfiguration.topLevelDestinations.contains(destination.id)
+            if (!isTopLevelDestination) {
+                binding.toolbarApp.setNavigationIcon(R.drawable.ic_back)
+            }
+            when (destination.id) {
+                R.id.homeFragment -> { binding.toolbarLogo.isVisible = true }
+                R.id.favoritesFragment -> { binding.toolbarLogo.isVisible = false}
+            }
+        }
     }
 }
