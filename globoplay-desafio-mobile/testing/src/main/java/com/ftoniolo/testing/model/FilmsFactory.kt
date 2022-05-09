05675fdb63199d033ee06519f9a757749a597b1a
Felipe Toniolo <ftoniolo.aat@gmail.com>
2022-05-08 21:11:26 -0300

    deleting comment in test

--- globoplay-desafio-mobile/testing/src/main/java/com/ftoniolo/testing/model/FilmsFactory.kt
+++ globoplay-desafio-mobile/testing/src/main/java/com/ftoniolo/testing/model/FilmsFactory.kt
@@ -35,19 +35,3 @@ class FilmsFactory {
         object DrEstranho : Movie()
     }
 }
-
-
-/*
-listOf(
-            FilmResponse(
-                id = 1L, overview = "Homem Aranha Overview", title = "Homem Aranha",
-                genreIds = listOf(9L, 8L, 7L), posterPath = "/imagem-homem-aranha",
-                releaseDate = "01/01/2022", originalLanguage = "pt-BR"
-            ),
-            FilmResponse(
-                id = 1L, overview = "Dr Estranho Overview", title = "Dr Estranho ",
-                genreIds = listOf(91L, 80L, 17L), posterPath = "/imagem-dr-estranho ",
-                releaseDate = "21/04/2022", originalLanguage = "pt-BR"
-            ),
-        )
- */
