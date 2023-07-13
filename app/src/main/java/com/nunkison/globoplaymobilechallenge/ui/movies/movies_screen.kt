8ff30c51613d04bd03b8b90a339ad9161055354e
Leonardo Braga Arnaud <buhtig@nunkison.com>
2023-07-13 15:31:24 -0400

    Medium Top app bar

--- app/src/main/java/com/nunkison/globoplaymobilechallenge/ui/movies/movies_screen.kt
+++ app/src/main/java/com/nunkison/globoplaymobilechallenge/ui/movies/movies_screen.kt
@@ -1,19 +1,31 @@
 package com.nunkison.globoplaymobilechallenge.ui.movies
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import com.nunkison.globoplaymobilechallenge.ui.Screen
+import com.nunkison.globoplaymobilechallenge.R
 import com.nunkison.globoplaymobilechallenge.ui.theme.GloboplayMobileChallengeTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MoviesScreen(navController: NavHostController) {
     Surface(
@@ -21,16 +33,39 @@ fun MoviesScreen(navController: NavHostController) {
         color = MaterialTheme.colorScheme.background
     ) {
         Column {
-            Text(
-                text = "MoviesScreen",
+            Scaffold(
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        colors = TopAppBarDefaults.mediumTopAppBarColors(
+                            containerColor = Color.Transparent,
+                        ),
+                        title = {
+                            Text(
+                                stringResource(id = R.string.app_name),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        },
+                    )
+                },
+                content = { innerPadding ->
+                    LazyColumn(
+                        contentPadding = innerPadding,
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        val list = (0..75).map { it.toString() }
+                        items(count = list.size) {
+                            Text(
+                                text = list[it],
+                                style = MaterialTheme.typography.bodyLarge,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(horizontal = 16.dp)
+                            )
+                        }
+                    }
+                },
             )
-            Button(onClick = {
-                navController.navigate(Screen.Favorites.route)
-            }) {
-                Text(
-                    text = "Next",
-                )
-            }
         }
     }
 }
