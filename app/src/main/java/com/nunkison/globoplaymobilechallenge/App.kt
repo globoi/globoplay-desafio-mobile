3f89d2edeb12b7319b55182dd8943719edb8032a
Leonardo Braga Arnaud <buhtig@nunkison.com>
2023-07-15 03:02:44 -0400

    fix commit

--- app/src/main/java/com/nunkison/globoplaymobilechallenge/App.kt
+++ app/src/main/java/com/nunkison/globoplaymobilechallenge/App.kt
@@ -1,14 +1,23 @@
 package com.nunkison.globoplaymobilechallenge
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.GlobalContext.startKoin
 
-class App: Application() {
+class App : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        startKoin{
+        current = this
+
+        startKoin {
+            androidContext(this@App)
             modules(androidModule)
         }
+    }
+
+    companion object{
+        private var current: App? = null
+        val instance: App get() = current!!
     }
 }
