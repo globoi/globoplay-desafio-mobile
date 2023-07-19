eebbe1c027fbcf4344cfd6b79d4dac3f35b1e967
Leonardo Braga Arnaud <buhtig@nunkison.com>
2023-07-19 04:06:20 -0400

    iniciando refactoring para viabilizar testes unitarios

---
+++ app/src/test/java/com/nunkison/globoplaymobilechallenge/MoviesFavoriteTest.kt
@@ -0,0 +1,12 @@
+package com.nunkison.globoplaymobilechallenge
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+
+class MoviesFavoriteTest {
+
+    @Test
+    fun `test toogle favorite`() = runBlocking {
+
+    }
+}
