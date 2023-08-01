7a8e66b8d1e47526d0f222541548527e233e27b7
Thalles Araújo <thalleshnaraujo@brq.com>
2023-08-01 16:47:48 -0300

    build: adiciona chaves de API TMDB

---
+++ DesafioGloboplay/DesafioGloboplay/Constants/AuthenticationConstants.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationConstants.swift
+//  DesafioGloboplay
+//
+//  Created by Thalles Araújo on 26/07/23.
+//
+
+import Foundation
+
+enum auth{
+    
+    enum TMDBAPI: String{
+        
+        case bearerToken = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzY2U4YWMyYmE4NzUyZWYzMjBiZmFkNTNlNGRkNjM1NyIsInN1YiI6IjVkNjE3MTExNmRlYTNhNzQ2YjkzNDU0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ZuBzHav_jCgMNnWf_S1ufJqNvr3PFnfmK_FKkgpNsEg"
+    }
+    
+}
