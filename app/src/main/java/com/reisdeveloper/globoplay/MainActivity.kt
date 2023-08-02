f65b33a47f593821e45acdda6eca1ca1878dfd27
reis756 <rodrigo_reis756@outlook.com>
2023-08-01 23:55:29 -0300

    feat: implemented Splash Screen and default app icon

--- app/src/main/java/com/reisdeveloper/globoplay/MainActivity.kt
+++ app/src/main/java/com/reisdeveloper/globoplay/MainActivity.kt
@@ -3,6 +3,7 @@ package com.reisdeveloper.globoplay
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -15,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
