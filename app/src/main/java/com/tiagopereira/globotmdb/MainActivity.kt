0910a5f4ce7237a7b4bf5fbc0c6f078f6e3911b1
Tiago Pereira <tiago_13710@hotmail.com>
2023-09-01 11:43:40 -0400

    removing compose

--- app/src/main/java/com/tiagopereira/globotmdb/MainActivity.kt
+++ app/src/main/java/com/tiagopereira/globotmdb/MainActivity.kt
@@ -1,46 +1,11 @@
 package com.tiagopereira.globotmdb
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.tiagopereira.globotmdb.ui.theme.GloboTMDBTheme
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            GloboTMDBTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    GloboTMDBTheme {
-        Greeting("Android")
+        setContentView(R.layout.activity_main)
     }
 }
